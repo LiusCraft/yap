d3fe72163cf5de8f0a3f2212e995bdc8ee9954df
LiusCraft <liuscraft@qq.com>
2024-02-01 15:04:28 +0800

    chore: add nested template example

--- demo/classfile_blog/gop_autogen.go
+++ demo/classfile_blog/gop_autogen.go
@@ -10,14 +10,16 @@ type blog struct {
 //line demo/classfile_blog/blog_yap.gox:1
 func (this *blog) MainEntry() {
 //line demo/classfile_blog/blog_yap.gox:1:1
-	this.Get("/p/:id", func(ctx *yap.Context) {
+	this.Template("*.*", "**/*.html")
 //line demo/classfile_blog/blog_yap.gox:2:1
+	this.Get("/p/:id", func(ctx *yap.Context) {
+//line demo/classfile_blog/blog_yap.gox:3:1
 		ctx.Yap__1("article", map[string]string{"id": ctx.Param("id")})
 	})
-//line demo/classfile_blog/blog_yap.gox:7:1
+//line demo/classfile_blog/blog_yap.gox:8:1
 	this.Run(":8080")
 }
 func main() {
-//line demo/classfile_blog/blog_yap.gox:7:1
+//line demo/classfile_blog/blog_yap.gox:8:1
 	yap.Gopt_App_Main(new(blog))
 }
